9e6db709c6485a090386a576cd9e588303c748b7
Augustin Delporte <delporteaugustin@gmail.com>
2016-09-06 20:45:41 -0400

    server running

---
+++ messing-around-server.js
@@ -0,0 +1,20 @@
+var http = require("http");
+
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<!DOCTYPE 'html'>");
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title>Hello World Page</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("<h1>");
+  response.write("hi!");
+  response.write("</h1>");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
+});
+
+server.listen(80);
+console.log("Server is listening");
