e25669dc5c6b2ac8743de66668807792d4397105
Augustin Delporte <delporteaugustin@gmail.com>
2016-10-03 09:37:18 -0400

    working solution

--- messing-around-server.js
+++ messing-around-server.js
@@ -15,7 +15,28 @@ var server = http.createServer(function(request, response) {
 
       </body>
 
-      <script type='text/javascript' src='script.js'></script>
+      <script>
+        var ele = document.getElementById("button");
+        ele.addEventListener("click", printIt, false);
+        function printIt() {
+         var node;
+         var textnode;
+         for (var i = 1; i <= 100; i++) {
+           node = document.createElement("LI");
+          if (i % 3 === 0 && i % 5 === 0) {
+           textnode = document.createTextNode("FizzBuzz");  
+          } else if (i % 3 === 0) {
+           textnode = document.createTextNode("Fizz");  
+          } else if (i % 5 === 0) {
+           textnode = document.createTextNode("Buzz");  
+          } else {
+           textnode = document.createTextNode(i);  
+          }
+          node.appendChild(textnode);
+          document.getElementById("fuzz").appendChild(node); 
+         }
+        };
+      </script>
     </html>`);
   response.end();
 });
