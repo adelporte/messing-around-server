cedaa083bf65479a3ada2379fc46d68285564485
Augustin Delporte <delporteaugustin@gmail.com>
2016-10-17 22:40:55 -0400

    worked on a workaround

--- messing-around-server.js
+++ messing-around-server.js
@@ -1,6 +1,33 @@
 var http = require("http");
 
+
 var server = http.createServer(function(request, response) {
+  console.log(request.url);
+  if (request.url === '/script.js') {
+    response.writeHead(200, {"Content-Type": "text/javascript"});
+    response.write(`
+    var ele = document.getElementById("button");
+      ele.addEventListener("click", printIt, false);
+      function printIt() {
+        var node;
+        var textnode;
+        for (var i = 1; i <= 100; i++) {
+          node = document.createElement("LI");
+        if (i % 3 === 0 && i % 5 === 0) {
+          textnode = document.createTextNode("FizzBuzz");  
+        } else if (i % 3 === 0) {
+          textnode = document.createTextNode("Fizz");  
+        } else if (i % 5 === 0) {
+          textnode = document.createTextNode("Buzz");  
+        } else {
+          textnode = document.createTextNode(i);  
+        }
+        node.appendChild(textnode);
+        document.getElementById("fuzz").appendChild(node); 
+        }
+      };
+    `);
+  };
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write(`<!DOCTYPE 'html'>
     <html>
@@ -12,33 +39,9 @@ var server = http.createServer(function(request, response) {
         </ul>
 
         <button id='button'>CLICK</button>
-
+        <script type="text/javascript" src="script.js"></script>
       </body>
-
-      <script>
-        var ele = document.getElementById("button");
-        ele.addEventListener("click", printIt, false);
-        function printIt() {
-         var node;
-         var textnode;
-         for (var i = 1; i <= 100; i++) {
-           node = document.createElement("LI");
-          if (i % 3 === 0 && i % 5 === 0) {
-           textnode = document.createTextNode("FizzBuzz");  
-          } else if (i % 3 === 0) {
-           textnode = document.createTextNode("Fizz");  
-          } else if (i % 5 === 0) {
-           textnode = document.createTextNode("Buzz");  
-          } else {
-           textnode = document.createTextNode(i);  
-          }
-          node.appendChild(textnode);
-          document.getElementById("fuzz").appendChild(node); 
-         }
-        };
-      </script>
     </html>`);
-  response.end();
 });
 
 
