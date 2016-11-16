853c6229996a9878390afafbb1206a32be601a01
Augustin Delporte <delporteaugustin@gmail.com>
2016-11-16 10:12:49 -0500

    AJAX call

--- messing-around-server.js
+++ messing-around-server.js
@@ -2,8 +2,8 @@ var http = require("http");
 var fs = require('fs');
 
 var server = http.createServer(function(request, response) {
-
-  //--The FizzBuzz logic
+  
+  /*--The FizzBuzz logic
       function generateFizzBuzz() {
         var values = [];
         for (var i = 1; i <= 100; i++) {
@@ -21,22 +21,62 @@ var server = http.createServer(function(request, response) {
       }
 
       var values = generateFizzBuzz();
-      var valuesJSON = JSON.stringify(values);
-      //--
-/*
+      //var valuesJSON = JSON.stringify(values);
+      */
+
 if (request.url === '/json.json') {
+
+  //--The FizzBuzz logic
+  function generateFizzBuzz() {
+    var values = [];
+    for (var i = 1; i <= 100; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        values.push("FizzBuzz");
+      } else if (i % 3 === 0) {
+        values.push("Fizz"); 
+      } else if (i % 5 === 0) {
+        values.push("Buzz");  
+      } else {
+        values.push(i);
+      }
+    }
+    return values;
+  }
+  
+  var values = generateFizzBuzz();
+
+  //--
+
   response.writeHead(200, {"Content-Type": "application/json"});
   var values = JSON.stringify({ 
-    valuesdata : values
+    values
   });
-  response.end(json);
-} else*/ if (request.url === '/script.js') {
+
+  response.end(values);
+  
+} else if (request.url === '/script.js') {
     response.writeHead(200, {"Content-Type": "application/javascript"});
-    response.write("values = " + valuesJSON);
+    //response.write("values = " + valuesJSON);
     response.write(`
       var ele = document.getElementById("button");
       ele.addEventListener("click", clickEventHandler, false);
       
+      var values;
+
+      //AJAX call
+      $.ajax({
+        url:'/json.json',
+        //If success
+        success : function (result) {
+          values = result.values;
+        },
+        //If error
+        error : function (error) {
+          console.log(error);
+        }
+      });
+
+
       //--The doer function
       function clickEventHandler() {
         renderFizzBuzz(values);
@@ -63,6 +103,7 @@ if (request.url === '/json.json') {
     <html>
       <head>
         <title>Hello World Page</title>
+        <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
       </head>
       <body>
         <ul id='fuzz'>
